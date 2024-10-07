b2ee1e749109449dd385059a72568e25f9c354ec
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2024-10-07 14:15:43 -0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,8 +1,15 @@
 <?php
+    session_start();
+    $is_logged_in = isset($_SESSION['user_id']);
     include '../templates/header.php';
 ?>
 <div class="container-fluid">
-    <h3 class="text-center">Encontre o seu digimon!</h3>
+    <h3 class="text-center">Encontre o seu Digimon!</h3>
+    <?php if (!$is_logged_in): ?>
+        <div class="alert alert-warning text-center">
+            <p>Por favor, <a href="login.php">faça login</a> para acessar todas as funcionalidades, como favoritar Digimons.</p>
+        </div>
+    <?php endif; ?>
     <div class="row justify-content-center my-4">
         <div class="col-md-4">
             <div class="card p-3 shadow-sm">
@@ -37,6 +44,11 @@
         include '../src/search.php';
         include '../src/result.php';
     ?>
+    <?php if ($is_logged_in): ?>
+        <div class="text-center mt-4">
+            <a href="favoritos.php" class="btn btn-success">Ver meus Digimons Favoritos</a>
+        </div>
+    <?php endif; ?>
 </div>
 <?php
     include '../templates/footer.php';

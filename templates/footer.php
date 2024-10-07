82b5da09d011591106b2a475f7078e7a5f716249
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2024-10-07 12:14:36 -0300

    Update footer.php

---
+++ templates/footer.php
@@ -0,0 +1,6 @@
+        <footer class="container">
+            <p>&copy; 2024 Digimon Search</p>
+        </footer>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-zBd4YYbYdwfdqP+JaeQs9lVRFN4v91iOBytMR6ZoCbPV1f5xgpImXK5a8d0nTc2d" crossorigin="anonymous"></script>
+    </body>
+</html>

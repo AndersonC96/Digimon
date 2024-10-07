dc53069e598d2c78cac2fcad88feb7d4810a9e73
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2024-10-07 14:12:53 -0300

    Create dashboard.php

---
+++ public/dashboard.php
@@ -0,0 +1,14 @@
+<?php
+    session_start();
+    if (!isset($_SESSION['user_id'])) {
+        header("Location: login.php");
+        exit();
+    }
+    include '../templates/header.php';
+?>
+<div class="container mt-5">
+    <h2>Bem-vindo, <?php echo $_SESSION['username']; ?>!</h2>
+    <p>Esta é a sua área restrita. Aproveite!</p>
+    <a href="logout.php" class="btn btn-danger">Sair</a>
+</div>
+<?php include '../templates/footer.php'; ?>

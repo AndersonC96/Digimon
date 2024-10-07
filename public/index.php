0c1f76db016c5e6d18d82d1779d15633d4aeb02e
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2024-10-07 18:02:11 -0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,54 +1,49 @@
 <?php
     session_start();
-    $is_logged_in = isset($_SESSION['user_id']);
     include '../templates/header.php';
 ?>
 <div class="container-fluid">
     <h3 class="text-center">Encontre o seu Digimon!</h3>
-    <?php if (!$is_logged_in): ?>
-        <div class="alert alert-warning text-center">
-            <p>Por favor, <a href="login.php">faça login</a> para acessar todas as funcionalidades, como favoritar Digimons.</p>
-        </div>
-    <?php endif; ?>
     <div class="row justify-content-center my-4">
-        <div class="col-md-4">
-            <div class="card p-3 shadow-sm">
-                <h5 class="card-title">Buscar por Nome</h5>
+        <div class="col-md-8">
+            <div class="card p-4 shadow-sm">
+                <h5 class="card-title">Pesquisar Digimon</h5>
                 <form action="" method="POST">
-                    <div class="form-group">
-                        <label for="nomeDigimon">Nome do Digimon:</label>
-                        <div class="input-group">
-                            <input type="text" class="form-control" id="nomeDigimon" name="txtDigimon" required>
-                            <button type="submit" name="btn_pesq_nome" class="btn btn-primary ms-2">Pesquisar</button>
+                    <div class="row">
+                        <div class="col-md-4 mb-3">
+                            <label for="nomeDigimon" class="form-label">Nome do Digimon:</label>
+                            <input type="text" class="form-control" id="nomeDigimon" name="txtDigimon" placeholder="Ex: Agumon">
+                        </div>
+                        <div class="col-md-4 mb-3">
+                            <label for="nivelDigimon" class="form-label">Nível do Digimon:</label>
+                            <select class="form-select" id="nivelDigimon" name="txtNivel">
+                                <option value="">Todos</option>
+                                <option value="In Training">In Training</option>
+                                <option value="Rookie">Rookie</option>
+                                <option value="Champion">Champion</option>
+                                <option value="Ultimate">Ultimate</option>
+                                <option value="Mega">Mega</option>
+                            </select>
+                        </div>
+                        <div class="col-md-4 mb-3">
+                            <label for="tipoDigimon" class="form-label">Tipo do Digimon:</label>
+                            <select class="form-select" id="tipoDigimon" name="txtTipo">
+                                <option value="">Todos</option>
+                                <option value="Vaccine">Vaccine</option>
+                                <option value="Virus">Virus</option>
+                                <option value="Data">Data</option>
+                                <option value="Free">Free</option>
+                            </select>
                         </div>
                     </div>
-                </form>
-            </div>
-        </div>
-        <div class="col-md-4">
-            <div class="card p-3 shadow-sm">
-                <h5 class="card-title">Buscar por Nível</h5>
-                <form action="" method="POST">
-                    <div class="form-group">
-                        <label for="nivelDigimon">Nível do Digimon:</label>
-                        <div class="input-group">
-                            <input type="text" class="form-control" id="nivelDigimon" name="txtNivel" required>
-                            <button type="submit" name="btn_pesq_nivel" class="btn btn-primary ms-2">Pesquisar</button>
-                        </div>
-                    </div>
+                    <button type="submit" name="btn_pesquisa" class="btn btn-primary">Pesquisar</button>
                 </form>
             </div>
         </div>
     </div>
     <?php
         include '../src/search.php';
-        include '../src/result.php';
     ?>
-    <?php if ($is_logged_in): ?>
-        <div class="text-center mt-4">
-            <a href="favoritos.php" class="btn btn-success">Ver meus Digimons Favoritos</a>
-        </div>
-    <?php endif; ?>
 </div>
 <?php
     include '../templates/footer.php';

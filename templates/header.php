0aabc5fe0cde03b56cdd75a697b92e6924d3e2c5
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2024-10-07 12:41:04 -0300

    Update header.php

--- templates/header.php
+++ templates/header.php
@@ -5,17 +5,14 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Digimon</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
         <script src="https://kit.fontawesome.com/3d7779fa7f.js" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="../public/style.css">
-        <link href="https://www.pngplay.com/wp-content/uploads/7/Digimon-Logo-PNG-Clipart-Background.png" rel="icon" type="image/x-icon" />
-        <script>
-            if (window.history.replaceState) {
-                window.history.replaceState(null, null, window.location.href);
-            }
-        </script>
+        <link rel="icon" href="data:image/x-icon;base64,...">
     </head>
     <body>
-        <header class="container">
-            <h1>Bem-vindo ao Digimon Search</h1>
+        <header class="bg-dark text-light py-3">
+            <div class="container">
+                <h1 class="text-center">Bem-vindo ao Digimon Search</h1>
+            </div>
         </header>

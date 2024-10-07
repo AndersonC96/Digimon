d166106d32602141b32bd4f1bde241e52ccd2ef1
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2024-10-07 12:52:12 -0300

    Update config.php

--- config/config.php
+++ config/config.php
@@ -2,3 +2,11 @@
     return [
         'digimon_api_url' => 'https://digimon-api.vercel.app/api/digimon',
     ];
+    $host = 'localhost';
+    $dbname = 'digimon_db';
+    $username = 'root';
+    $password = '';
+    $conn = new mysqli($host, $username, $password, $dbname);
+    if ($conn->connect_error) {
+        die("Conexão com o banco de dados falhou: " . $conn->connect_error);
+    }
